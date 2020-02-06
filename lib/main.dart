a266c5c63ff988d358eb17bc301186fd2b8ecbf7
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2020-02-06 23:51:32 +0530

    profile pic added

--- lib/main.dart
+++ lib/main.dart
@@ -35,30 +35,63 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Image.network(
-                'https://i0.wp.com/zeagwat.com/wp-content/uploads/2018/10/Web-Design.png?fit=624%2C611&ssl=1'),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+            Row(
               children: <Widget>[
-                Text(
-                  'I am Barry Allen',
-                  style: TextStyle(
-                      fontSize: 40,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
                 Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Text(
-                    'Web Developer',
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold),
+                  padding: const EdgeInsets.only(right: 40.0),
+                  child: Image.network(
+                    'https://upload.wikimedia.org/wikipedia/commons/4/43/Paul_Circle.png',
+                    height: 200,
+                    width: 200,
                   ),
                 ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'I am Barry Allen',
+                      style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'Web Developer',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            'Skills- ',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            'Angular, JQuery, PHP, Dart',
+                            style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ],
-            )
+            ),
+            Image.network(
+                'https://i0.wp.com/zeagwat.com/wp-content/uploads/2018/10/Web-Design.png?fit=624%2C611&ssl=1'),
           ],
         ),
       ),
